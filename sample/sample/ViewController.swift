5a7a049014460308434f1cf45b88b5299112b783
Oliver Michalak <oliver.michalak@kreait.com>
2018-04-03 20:03:25 +0200

    add sample project

---
+++ sample/sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by kreait on 03.04.18.
+//  Copyright © 2018 kreait. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
